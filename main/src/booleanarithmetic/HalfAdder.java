078076b45cfa77e94da5152c325615fc6973f2c6
Laurent Kloeble <lkloeble@gmail.com>
2017-09-16 22:57:10 +0200

    half and full adders;

---
+++ main/src/booleanarithmetic/HalfAdder.java
@@ -0,0 +1,7 @@
+package booleanarithmetic;
+
+/**
+ * Created by Laurent KLOEBLE on 16/09/2017.
+ */
+public class HalfAdder {
+}
---
+++ main/src/booleanarithmetic/PairValue.java
@@ -0,0 +1,12 @@
+package booleanarithmetic;
+
+import booleanlogic.GateValue;
+
+/**
+ * Created by Laurent KLOEBLE on 16/09/2017.
+ */
+public class PairValue {
+
+    private GateValue carry;
+    private GateValue sum;
+}
